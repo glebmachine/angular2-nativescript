68db313f007cb14adee0d5c547e047641d769cbd
Gleb Mikheev <mikheev@betaagency.ru>
2016-08-21 14:38:39 +0300

    chapter 3.4

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,23 +1,54 @@
 import { Component } from "@angular/core";
+import { User } from "./shared/user/user";
+import { UserService } from "./shared/user/user.service";
 
 @Component({
   selector: "my-app",
-  template: `
-    <StackLayout>
-      <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
-
-      <TextField hint="Email Address" keyboardType="email"
-        autocorrect="false" autocapitalizationType="none"></TextField>
-      <TextField hint="Password" secure="true"></TextField>
-
-      <Button text="Sign in" class="submit-button"></Button>
-      <Button text="Sign up for Groceries"></Button>
-    </StackLayout>
-  `,
+  templateUrl: "pages/login/login.html",
+  providers: [
+    UserService,
+  ],
   styleUrls: [
     "pages/login/login-common.css",
     "pages/login/login.css"
   ]
 })
 
-export class AppComponent { }
+export class AppComponent {
+  user: User;
+  isLoggingIn = false;
+
+  constructor(private _userService: UserService) {
+    this.user = new User();
+    this.user.email = "user@nativescript.org";
+    this.user.password = "password";
+  }
+
+  submit() {
+    if (this.isLoggingIn) {
+      this.login();
+    } else {
+      this.signUp();
+    }
+  }
+
+  login() {
+    // TODO: Define
+  }
+
+  signUp() {
+    this._userService.register(this.user)
+      .subscribe(
+        () => {
+          alert("Your account was successfully created.");
+          this.toggleDisplay();
+        },
+        () => alert("Unfortunately we were unable to create your account.")
+      );
+  }
+
+  toggleDisplay() {
+    this.isLoggingIn = !this.isLoggingIn;
+  }
+
+}
